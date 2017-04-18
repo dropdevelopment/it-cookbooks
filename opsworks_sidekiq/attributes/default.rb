ff9fba881aedac5edbe481a02f0a4f9233e6d0d0
djgable47 <vann@pmg2.com>
2017-04-18 12:18:28 -0700

    init

---
+++ opsworks_sidekiq/attributes/default.rb
@@ -0,0 +1,14 @@
+include_attribute 'deploy'
+
+default[:sidekiq] = {}
+
+node[:deploy].each do |application, deploy|
+  default[:sidekiq][application.intern] = {}
+  default[:sidekiq][application.intern][:restart_command] = "sudo monit restart -g sidekiq_#{application}_group"
+  default[:sidekiq][application.intern][:syslog] = false
+  override['sidekiq']['instant_api']['staging']['process_count'] = 4
+  override['sidekiq']['instant_api']['staging']['config']['concurrency'] = 20
+  override['sidekiq']['instant_api']['staging']['config']['verbose'] = false
+  override['sidekiq']['instant_api']['staging']['config']['queues'] = ['default', 'mailers', 'devise_queue']
+  override['sidekiq']['instant_api']['staging']['config']['logfile'] = "./log/sidekiq.log"
+end
